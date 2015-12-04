a5d40a61ff80d4be0b35f2ebdffbae9340d555a9
kartikvempati <kvempati11@gmail.com>
2015-12-04 15:44:11 -0800

    Create randomized enemies

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,29 @@
 // start slingin' some d3 here.
+var svg = d3.select('body').append('svg')
+   .attr("width", 500)
+   .attr("height", 500)
+   .attr("class", "svg");
+
+
+var dataset = [];
+for(var i =0; i < 50; i++ ) {
+  dataset[i] = [Math.random()*500, Math.random()*500];
+}   
+
+var enemies = svg.selectAll('circle')
+                 .data(dataset)
+                 .enter()
+                 .append("svg:circle")
+                 .attr('cx', function(d) {
+                  return d[0] + 'px';
+                 })
+                 .attr('cy', function(d) {
+                  return d[1] + 'px';
+                 })
+                 .attr('r', 10 + 'px')
+                 .attr("fill", "yellow")
+                 .attr("stroke", "orange")
+                 .attr("stroke-width", function(d) {
+                  return 5;
+                 })
+                 .attr('class', "circle");
