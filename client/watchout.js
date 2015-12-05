0b23dc9e0a162ae2f7e299d04bb70164ff7e3f70
kartikvempati <kvempati11@gmail.com>
2015-12-04 18:03:59 -0800

    Created draggable player

--- client/watchout.js
+++ client/watchout.js
@@ -4,7 +4,11 @@ var svg = d3.select('body').append('svg')
    .attr("height", 500);
   
 
+
+/* ENEMY DATA*/
+
 var dataset = [];
+
 
 function randomPos(dataset) {
   for(var i =0; i < 50; i++ ) {
@@ -13,9 +17,10 @@ function randomPos(dataset) {
   return dataset;
 }
 
+
 var initialPos = randomPos(dataset);
 
-var enemies = svg.selectAll('circle')
+var enemies = svg.selectAll('enemy')
                  .data(dataset)
                  .enter()
                  .append("svg:circle")
@@ -29,7 +34,27 @@ var enemies = svg.selectAll('circle')
                  .attr("fill", "yellow")
                  .attr("stroke", "orange")
                  .attr("stroke-width", 5);
-            
+
+
+/* PLAYER DATA */
+//Hoisting
+
+var drag = d3.behavior.drag()
+              .on('drag', function(){
+                player.attr('cx', d3.event.x + 'px')
+                      .attr('cy', d3.event.y + 'px');
+                });
+
+var player = svg.selectAll('player')
+            .data([250, 250])
+            .enter()
+            .append("svg:circle")
+            .attr('cx', 250 + 'px')
+            .attr('cy', 250 + 'px')
+            .attr('r', 10 + 'px')
+            .call(drag);
+
+
 
 var update = function(){
   enemies.data(randomPos(dataset))
@@ -42,5 +67,4 @@ var update = function(){
           })
 };
 
-setInterval(update, 1000);
-                 
+setInterval(update, 1000);                 
